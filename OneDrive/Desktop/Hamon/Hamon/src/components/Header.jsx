a6ef1b8318e2dbed27fc70911159de4af9174e15
PranavKarunan <pranavkarunant@gmail.com>
2024-02-15 12:53:45 +0530

    Navbar added

--- OneDrive/Desktop/Hamon/Hamon/src/components/Header.jsx
+++ OneDrive/Desktop/Hamon/Hamon/src/components/Header.jsx
@@ -6,10 +6,14 @@ function Header() {
             <div>Artisan Resto Cafe</div>
             <div className="order-part">
                 <span>My Orders</span>
-                <IconContext.Provider value={{ color: 'white', className: 'cart-icon', size: '1.4em' }} >
-                    <CiShoppingCart />
-                </IconContext.Provider>
-                <i className="fas fa-shopping-cart"></i>
+                <div className="cart">
+                    <div >
+                        <IconContext.Provider value={{ color: 'white', className: 'cart-icon', size: '1.4em' }} >
+                            <CiShoppingCart />
+                        </IconContext.Provider>
+                    </div>
+                    <div className="badge">0</div>
+                </div>
 
             </div>
         </div>
---
+++ OneDrive/Desktop/Hamon/Hamon/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+function Navbar() {
+    return (
+        <div className="nav-bar">
+            <ul className="nav-menu">
+                <li className="active">Salads and Soup</li>
+                <li>From The Barnyard</li>
+                <li>From The Hen House</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
